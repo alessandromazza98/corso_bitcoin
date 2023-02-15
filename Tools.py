49035aacb14802c9790d7e3fb88c5b7a2a01b866
alessandromazza <alessandro98.mazza@gmail.com>
2023-02-15 12:51:03 +0100

    Tool class done

--- Tools.py
+++ Tools.py
@@ -1,29 +1,40 @@
-# --------
-# Transform a byte input into its int representation
-# --------
+from hashlib import sha256
+
+
 def int_from_bytes(b: bytes) -> int:
+    """Transform a byte input into its int representation"""
     return int.from_bytes(b, byteorder="big")
 
 
-# --------
-# Transform an int input into its byte representation
-# --------
 def bytes_from_int(x: int) -> bytes:
+    """Transform an int input into its byte representation"""
     return x.to_bytes(32, byteorder="big")
 
 
-# --------
-# Reverse byte order
-# --------
 def reverse_byte_order(b: bytes) -> bytes:
+    """Reverse byte order"""
     return b[::-1]
 
 
-def len_encoding(data: bytes) -> bytes:
-    len_bytes = bytes_from_int(len(data))
-    if len_bytes < b'\xfc':
-        return len_bytes[-1:]
-    elif len_bytes < b'\xff\xff':
-        return b'\xfd' + len_bytes[-2:]
-    elif len_bytes < b'\xff\xff\xff\xff':
+def compact_size(data: bytes) -> bytes:
+    """Return the compact size of the data, that is a byte representing an integer"""
+    len_data = len(data)
+    len_data_bytes = bytes_from_int(len_data)
+    if len_data < 252:
+        return len_data_bytes[-1:]
+    elif len_data < 65535:
+        return b'\xfd' + len_data_bytes[-2:]
+    elif len_data < 4294967295:
+        return b'\xfe' + len_data_bytes[-4:]
+    elif len_data < 18446744073709551615:
+        return b'\xff' + len_data_bytes[-8:]
 
+
+def sha256_2(data: bytes) -> bytes:
+    """sha256^2(data)"""
+    return sha256(sha256(data).digest()).digest()
+
+
+if __name__ == '__main__':
+    print("hello")
+    
